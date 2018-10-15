1b60389ba269955f05d1748bc14dc3db74233f7c
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2018-10-15 13:09:53 -0300

    config: simplify the way configs are exposed

--- packages/api/config/database.js
+++ packages/api/config/database.js
@@ -1,33 +1,9 @@
-const { getConfig } = require('./index')
-
-const config = {
-  development: {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    logging: false,
-  },
-  production: {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    logging: false,
-  },
-  test: {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    logging: false,
-  },
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  logging: false,
 }
-
-module.exports = getConfig(config)
--- packages/api/config/index.js
+++
@@ -1,7 +0,0 @@
-const { prop } = require('ramda')
-
-const getConfig = config => prop(process.env.NODE_ENV, config)
-
-module.exports = {
-  getConfig,
-}
